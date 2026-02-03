9b302e9fa7910d2cade2b0900471a86be8c481f6
JesúsÁngelAlcaláPablo <jesusangel.s441233@cesurformacion.com>
2026-02-03 10:25:19 +0100

    Crear y Editar añadidos

--- src/main/java/org/example/web_springboot/WebController.java
+++ src/main/java/org/example/web_springboot/WebController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -44,5 +45,33 @@ class WebController {
         }
     }
 
+    @GetMapping("/juego/nuevo")
+    public String nuevoJuegoForm(Model model) {
+        model.addAttribute("game", new Game());
+        return "form";
+    }
 
+    @PostMapping("/juego/nuevo")
+    public String nuevoJuego(Game game) {
+        gameRepository.save(game);
+        return "redirect:/";
+    }
+
+    @GetMapping("/juego/editar/{id}")
+    public String editarJuegoForm(@PathVariable Integer id, Model model) {
+        Optional<Game> gameOptional = gameRepository.findById(id);
+        if (gameOptional.isPresent()) {
+            model.addAttribute("game", gameOptional.get());
+            return "form";
+        } else {
+            model.addAttribute("error", "No se ha encontrado el juego con ID: " + id);
+            return "error";
+        }
+    }
+
+    @PostMapping("/juego/editar/{id}")
+    public String editarJuego(Game game) {
+        gameRepository.save(game);
+        return "redirect:/";
+    }
 }
